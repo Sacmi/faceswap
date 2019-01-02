4760d7f73b8a4dd3e3cf485ad9f252318f160fec
Sacmi <sacmi@pm.me>
2019-01-02 18:21:00 +0700

    Improved Google Drive uploader

--- lib/gdrivesync.py
+++ lib/gdrivesync.py
@@ -1,15 +1,19 @@
-from tqdm import tqdm
+import threading
 from os import listdir
+
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-import threading
+
+from tqdm import tqdm
+
 
 class GoogleDriveSync:
     """ Simple PyDrive wrapper for sync model """
-    def __init__(self, model_dir, key="gdrive_key"):
+
+    def __init__(self, model_dir, key="sample_gdrive_key"):
         """ Log in to Google Drive account using OAuth key """
         auth = GoogleAuth()
-        if key == "gdrive_key":
+        if key == "sample_gdrive_key":
             message = "Please, get OAuth key and start \"faceswap.py\" with flag -gd <your key>"
             raise GoogleDriveAuthError(
                 message)
@@ -22,7 +26,7 @@ class GoogleDriveSync:
                 message = "Something wrong with your OAuth key. Try to get new one and restart this script"
                 raise GoogleDriveAuthError(
                     message)
-        
+
         self.drive = GoogleDrive(auth)
         self.model_dir = model_dir
         self.is_activated = True
@@ -30,17 +34,19 @@ class GoogleDriveSync:
     def upload(self):
         """ Upload model to Google Drive """
         if self.is_activated:
-            file_list = self.drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+            file_list = self.drive.ListFile(
+                {'q': "'root' in parents and trashed=false"}).GetList()
 
             for h5file in tqdm(listdir(self.model_dir)):
                 for gd_file in file_list:
                     if h5file == gd_file["title"]:
-                        file = self.drive.CreateFile({ "id": gd_file["id"] })
-                    
-                if "file" not in locals() and h5file.endswith(".h5"):
-                    file = self.drive.CreateFile({ "title": h5file })
+                        file = self.drive.CreateFile({"id": gd_file["id"]})
 
-                if "file" in locals():            
+                if "file" not in locals():
+                    if h5file.endswith(".json") or h5file.endswith(".h5"):
+                        file = self.drive.CreateFile({"title": h5file})
+
+                if "file" in locals():
                     file.SetContentFile("{}/{}".format(self.model_dir, h5file))
                     file.Upload()
 
@@ -51,6 +57,7 @@ class GoogleDriveSync:
         thread = threading.Thread(target=self.upload, name=thread_name)
         thread.start()
         return thread
+
 
 class GoogleDriveAuthError(BaseException):
     pass
